808abc49212d33f80f2bb60d698af5d37b1a4429
MrKrrot <rafita.olguin@hotmail.com>
2024-02-25 12:57:56 -0600

    fix(api): :bug: server error now returns 404 when pokemon not found instead of status 500

--- src/services/pokemon/findPokemonById.ts
+++ src/services/pokemon/findPokemonById.ts
@@ -3,8 +3,12 @@ import { ServerError } from '@utils'
 export const findPokemonById = async (id: string) => {
   const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
 
+  if (response.status === 404) {
+    throw new ServerError('Pokemon not found', 404)
+  }
+
   if (!response.ok) {
-    throw new ServerError('Failed to fetch pokemon', 500)
+    throw new ServerError('Internal server error', 500)
   }
 
   const data = await response.json()
