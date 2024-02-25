e3ea6ca7d3915f01bba7ba3413fc6dad0203129c
MrKrrot <rafita.olguin@hotmail.com>
2024-02-25 13:45:07 -0600

    chore(api): :wrench: delete configuration build with jest in typescript

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,11 +1,6 @@
 import 'dotenv/config'
 
-const {
-  DB_URI: C_DB_URI,
-  DB_URI_TEST: C_DB_URI_TEST,
-  NODE_ENV: C_NODE_ENV,
-  PORT: C_PORT
-} = process.env
+const { DB_URI: C_DB_URI, NODE_ENV: C_NODE_ENV, PORT: C_PORT } = process.env
 
 const ensureEnv = (env: string | undefined, name: string) => {
   if (!env) throw new Error(`${name} env is not defined`)
@@ -14,8 +9,7 @@ const ensureEnv = (env: string | undefined, name: string) => {
 }
 
 const DB_URI = ensureEnv(C_DB_URI, 'DB_URI')
-const DB_URI_TEST = ensureEnv(C_DB_URI_TEST, 'DB_URI_TEST')
 const NODE_ENV = ensureEnv(C_NODE_ENV, 'NODE_ENV')
 const PORT = ensureEnv(C_PORT, 'PORT')
 
-export { DB_URI, DB_URI_TEST, NODE_ENV, PORT }
+export { DB_URI, NODE_ENV, PORT }
